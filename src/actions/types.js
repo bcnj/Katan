16ea8771059f6dc865bab68468b6e4357ed30e23
jMuzsik <jerrymuzsik@icloud.com>
2017-11-21 15:50:58 -0500

    Update types.js

--- src/actions/types.js
+++ src/actions/types.js
@@ -7,5 +7,5 @@ export const END_GAME = 'END_GAME'
 export const SET_PLAYER = 'SET_PLAYER'
 export const SET_USER = 'SET_USER'
 
-const GET_TILES = 'GET_TILES'
-const SET_ROBBER = 'SET_ROBBER'
+export const GET_TILES = 'GET_TILES'
+export const SET_ROBBER = 'SET_ROBBER'
