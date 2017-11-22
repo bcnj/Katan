d410e66494d3ed75812064dd114441708803b57d
Nicholas Liu <hkai.liu@gmail.com>
2017-11-22 11:14:40 -0500

    fix: devCard Int to Str

--- src/dummyData.js
+++ src/dummyData.js
@@ -48,193 +48,193 @@ const dummyData = {
     }
   },
   "devCards": {
-    1: {
+    '1': {
       "name": "Knight"
     },
-    2: {
+    '2': {
       "name": "Knight"
     },
-    3: {
+    '3': {
       "name": "Knight"
     },
-    4: {
+    '4': {
       "name": "Knight"
     },
-    5: {
+    '5': {
       "name": "Knight"
     },
-    6: {
+    '6': {
       "name": "Knight"
     },
-    7: {
+    '7': {
       "name": "Knight"
     },
-    8: {
+    '8': {
       "name": "Knight"
     },
-    9: {
+    '9': {
       "name": "Knight"
     },
-    10: {
+    '10': {
       "name": "Knight"
     },
-    11: {
+    '11': {
       "name": "Knight"
     },
-    12: {
+    '12': {
       "name": "Knight"
     },
-    13: {
+    '13': {
       "name": "Knight"
     },
-    14: {
+    '14': {
       "name": "Knight"
     },
-    15: {
+    '15': {
       "name": "Victory Point"
     },
-    16: {
+    '16': {
       "name": "Victory Point"
     },
-    17: {
+    '17': {
       "name": "Victory Point"
     },
-    18: {
+    '18': {
       "name": "Victory Point"
     },
-    19: {
+    '19': {
       "name": "Victory Point"
     },
-    20: {
+    '20': {
       "name": "Year of Plenty"
     },
-    21: {
+    '21': {
       "name": "Year of Plenty"
     },
-    22: {
+    '22': {
       "name": "Monopoly"
     },
-    23: {
+    '23': {
       "name": "Monopoly"
     },
-    24: {
+    '24': {
       "name": "Road Building"
     },
-    25: {
+    '25': {
       "name": "Road Building"
     }
   },
   "tileNodes": {
     1: {
-      "children": [1,4,5,8,9,13],
+      "children": [1, 4, 5, 8, 9, 13],
       "resource": "ORE",
       "robber": false,
       "rollNumber": 10
     },
     2: {
-      "children": [2,5,6,9,10,14],
+      "children": [2, 5, 6, 9, 10, 14],
       "resource": "SHEEP",
       "robber": false,
       "rollNumber": 2
     },
     3: {
-      "children": [3,6,7,10,11,15],
+      "children": [3, 6, 7, 10, 11, 15],
       "resource": "WOOD",
       "robber": false,
       "rollNumber": 9
     },
     4: {
-      "children": [12,8,13,17,18,23],
+      "children": [12, 8, 13, 17, 18, 23],
       "resource": "WHEAT",
       "robber": false,
       "rollNumber": 12
     },
     5: {
-      "children": [13,9,14,18,19,24],
+      "children": [13, 9, 14, 18, 19, 24],
       "resource": "BRICK",
       "robber": false,
       "rollNumber": 6
     },
     6: {
-      "children": [14,10,15,19,20,25],
+      "children": [14, 10, 15, 19, 20, 25],
       "resource": "SHEEP",
       "robber": false,
       "rollNumber": 4
     },
     7: {
-      "children": [15,11,16,20,21,26],
+      "children": [15, 11, 16, 20, 21, 26],
       "resource": "BRICK",
       "robber": false,
       "rollNumber": 10
     },
     8: {
-      "children": [22,17,23,28,29,34],
+      "children": [22, 17, 23, 28, 29, 34],
       "resource": "WHEAT",
       "robber": false,
       "rollNumber": 9
     },
     9: {
-      "children": [23,18,24,29,30,35],
+      "children": [23, 18, 24, 29, 30, 35],
       "resource": "WOOD",
       "robber": false,
       "rollNumber": 11
     },
     10: {
-      "children": [24,19,25,30,31,36],
+      "children": [24, 19, 25, 30, 31, 36],
       "resource": "DESERT",
       "robber": false,
       "rollNumber": 0
     },
     11: {
-      "children": [25,20,26,31,32,37],
+      "children": [25, 20, 26, 31, 32, 37],
       "resource": "WOOD",
       "robber": false,
       "rollNumber": 3
     },
     12: {
-      "children": [26,21,27,32,33,38],
+      "children": [26, 21, 27, 32, 33, 38],
       "resource": "ORE",
       "robber": false,
       "rollNumber": 8
     },
     13: {
-      "children": [34,29,35,39,40,44],
+      "children": [34, 29, 35, 39, 40, 44],
       "resource": "WOOD",
       "robber": false,
       "rollNumber": 8
     },
     14: {
-      "children": [35,30,36,40,41,45],
+      "children": [35, 30, 36, 40, 41, 45],
       "resource": "ORE",
       "robber": false,
       "rollNumber": 3
     },
     15: {
-      "children": [36,31,37,41,42,46],
+      "children": [36, 31, 37, 41, 42, 46],
       "resource": "WHEAT",
       "robber": false,
       "rollNumber": 4
     },
     16: {
-      "children": [37,32,38,42,43,47],
+      "children": [37, 32, 38, 42, 43, 47],
       "resource": "SHEEP",
       "robber": false,
       "rollNumber": 5
     },
     17: {
-      "children": [44,40,45,48,49,52],
+      "children": [44, 40, 45, 48, 49, 52],
       "resource": "BRICK",
       "robber": false,
       "rollNumber": 5
     },
     18: {
-      "children": [45,41,46,49,50,53],
+      "children": [45, 41, 46, 49, 50, 53],
       "resource": "WHEAT",
       "robber": false,
       "rollNumber": 6
     },
     19: {
-      "children": [46,42,47,48,59,54],
+      "children": [46, 42, 47, 48, 59, 54],
       "resource": "SHEEP",
       "robber": false,
       "rollNumber": 11
@@ -484,133 +484,133 @@ const dummyData = {
       "settlement": false
     },
     4: {
-      "neighbors":[1, 8],
+      "neighbors": [1, 8],
       "player": 0,
       "city": false,
       "settlement": false
     },
     5: {
-      "neighbors":[1,2,9],
+      "neighbors": [1, 2, 9],
       "player": 0,
       "city": false,
       "settlement": false
     },
     6: {
-      "neighbors": [2,3,10],
+      "neighbors": [2, 3, 10],
       "player": 0,
       "city": false,
       "settlement": false
     },
     7: {
-      "neighbors": [3,11],
+      "neighbors": [3, 11],
       "player": 0,
       "city": false,
       "settlement": false
     },
     8: {
-      "neighbors": [4,12,13],
+      "neighbors": [4, 12, 13],
       "player": 0,
       "city": false,
       "settlement": false
     },
     9: {
-      "neighbors": [5,13,14],
+      "neighbors": [5, 13, 14],
       "player": 0,
       "city": false,
       "settlement": false
     },
     10: {
-      "neighbors": [6,14,15],
+      "neighbors": [6, 14, 15],
       "player": 0,
       "city": false,
       "settlement": false
     },
     11: {
-      "neighbors": [7,15,16],
+      "neighbors": [7, 15, 16],
       "player": 0,
       "city": false,
       "settlement": false
     },
     12: {
-      "neighbors": [8,17],
+      "neighbors": [8, 17],
       "player": 0,
       "city": false,
       "settlement": false
     },
     13: {
-      "neighbors": [8,9,18],
+      "neighbors": [8, 9, 18],
       "player": 0,
       "city": false,
       "settlement": false
     },
     14: {
-      "neighbors": [9,10,19],
+      "neighbors": [9, 10, 19],
       "player": 0,
       "city": false,
       "settlement": false
     },
     15: {
-      "neighbors": [10,11,20],
+      "neighbors": [10, 11, 20],
       "player": 0,
       "city": false,
       "settlement": false
     },
     16: {
-      "neighbors": [11,21],
+      "neighbors": [11, 21],
       "player": 0,
       "city": false,
       "settlement": false
     },
     17: {
-      "neighbors": [12,22,23],
+      "neighbors": [12, 22, 23],
       "player": 0,
       "city": false,
       "settlement": false
     },
     18: {
-      "neighbors": [13,23,24],
+      "neighbors": [13, 23, 24],
       "player": 0,
       "city": false,
       "settlement": false
     },
     19: {
-      "neighbors": [14,24,25],
+      "neighbors": [14, 24, 25],
       "player": 0,
       "city": false,
       "settlement": false
     },
     20: {
-      "neighbors": [15,25,26],
+      "neighbors": [15, 25, 26],
       "player": 0,
       "city": false,
       "settlement": false
     },
     21: {
-      "neighbors": [16,26,27],
+      "neighbors": [16, 26, 27],
       "player": 0,
       "city": false,
       "settlement": false
     },
     22: {
-      "neighbors": [17,28],
+      "neighbors": [17, 28],
       "player": 0,
       "city": false,
       "settlement": false
     },
     23: {
-      "neighbors": [17,18,29],
+      "neighbors": [17, 18, 29],
       "player": 0,
       "city": false,
       "settlement": false
     },
     24: {
-      "neighbors": [18,19,30],
+      "neighbors": [18, 19, 30],
       "player": 0,
       "city": false,
       "settlement": false
     },
     25: {
-      "neighbors": [19,20,31],
+      "neighbors": [19, 20, 31],
       "player": 0,
       "city": false,
       "settlement": false
@@ -622,37 +622,37 @@ const dummyData = {
       "settlement": false
     },
     27: {
-      "neighbors": [21,33],
+      "neighbors": [21, 33],
       "player": 0,
       "city": false,
       "settlement": false
     },
     28: {
-      "neighbors": [22,34],
+      "neighbors": [22, 34],
       "player": 0,
       "city": false,
       "settlement": false
     },
     29: {
-      "neighbors": [23,34,35],
+      "neighbors": [23, 34, 35],
       "player": 0,
       "city": false,
       "settlement": false
     },
     30: {
-      "neighbors": [24,35,36],
+      "neighbors": [24, 35, 36],
       "player": 0,
       "city": false,
       "settlement": false
     },
     31: {
-      "neighbors": [25,36,37],
+      "neighbors": [25, 36, 37],
       "player": 0,
       "city": false,
       "settlement": false
     },
     32: {
-      "neighbors": [26,37,38],
+      "neighbors": [26, 37, 38],
       "player": 0,
       "city": false,
       "settlement": false
@@ -664,103 +664,103 @@ const dummyData = {
       "settlement": false
     },
     34: {
-      "neighbors": [28,29,39],
+      "neighbors": [28, 29, 39],
       "player": 0,
       "city": false,
       "settlement": false
     },
     35: {
-      "neighbors": [29,20,40],
+      "neighbors": [29, 20, 40],
       "player": 0,
       "city": false,
       "settlement": false
     },
     36: {
-      "neighbors": [30,31,41],
+      "neighbors": [30, 31, 41],
       "player": 0,
       "city": false,
       "settlement": false
     },
     37: {
-      "neighbors": [31,32,42],
+      "neighbors": [31, 32, 42],
       "player": 0,
       "city": false,
       "settlement": false
     },
     38: {
-      "neighbors": [32,33,43],
+      "neighbors": [32, 33, 43],
       "player": 0,
       "city": false,
       "settlement": false
     },
     39: {
-      "neighbors": [34,44],
+      "neighbors": [34, 44],
       "player": 0,
       "city": false,
       "settlement": false
     },
     40: {
-      "neighbors": [35,44,45],
+      "neighbors": [35, 44, 45],
       "player": 0,
       "city": false,
       "settlement": false
     },
     41: {
-      "neighbors": [36,45,46],
+      "neighbors": [36, 45, 46],
       "player": 0,
       "city": false,
       "settlement": false
     },
     42: {
-      "neighbors": [37,46,47],
+      "neighbors": [37, 46, 47],
       "player": 0,
       "city": false,
       "settlement": false
     },
     43: {
-      "neighbors": [38,47],
+      "neighbors": [38, 47],
       "player": 0,
       "city": false,
       "settlement": false
     },
     44: {
-      "neighbors": [39,40,48],
+      "neighbors": [39, 40, 48],
       "player": 0,
       "city": false,
       "settlement": false
     },
     45: {
-      "neighbors": [40,41,49],
+      "neighbors": [40, 41, 49],
       "player": 0,
       "city": false,
       "settlement": false
     },
     46: {
-      "neighbors": [41,42,50],
+      "neighbors": [41, 42, 50],
       "player": 0,
       "city": false,
       "settlement": false
     },
     47: {
-      "neighbors": [42,43,51],
+      "neighbors": [42, 43, 51],
       "player": 0,
       "city": false,
       "settlement": false
     },
     48: {
-      "neighbors": [44,52],
+      "neighbors": [44, 52],
       "player": 0,
       "city": false,
       "settlement": false
     },
     49: {
-      "neighbors": [45,52,53],
+      "neighbors": [45, 52, 53],
       "player": 0,
       "city": false,
       "settlement": false
     },
     50: {
-      "neighbors": [46,53,54],
+      "neighbors": [46, 53, 54],
       "player": 0,
       "city": false,
       "settlement": false
@@ -772,19 +772,19 @@ const dummyData = {
       "settlement": false
     },
     52: {
-      "neighbors": [48,49],
+      "neighbors": [48, 49],
       "player": 0,
       "city": false,
       "settlement": false
     },
     53: {
-      "neighbors": [49,50],
+      "neighbors": [49, 50],
       "player": 0,
       "city": false,
       "settlement": false
     },
     54: {
-      "neighbors": [50,51],
+      "neighbors": [50, 51],
       "player": 0,
       "city": false,
       "settlement": false
