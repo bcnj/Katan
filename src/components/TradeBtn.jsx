36198726e7c516bdbc08b48dbc2cabd4e4faa69b
woskxn <iamchangxx@gmail.com>
2017-11-26 12:14:42 -0500

    fix: build and trade button

--- src/components/TradeBtn.jsx
+++ src/components/TradeBtn.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Form, Button, Header, Image, Modal, Grid } from 'semantic-ui-react'
 import wheat from '../images/wheat.jpg'
@@ -9,65 +9,65 @@ import ore from '../images/ore.jpg'
 
 const images = [wheat, brick, sheep, wood, ore]
 
-const TradeBtn = (props) => {
+class TradeBtn extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      open: false,
+    }
+    this.handleClose = this.handleClose.bind(this)
+    this.handleOpen = this.handleOpen.bind(this)
+  }
 
-  return (
-      <Modal trigger={<Button style={{ width: '49%', height: '75%' }}>Trade</Button>} >
-        <h1 style={{ textAlign: 'center' }}> I am offering </h1>
-        <Grid divided='vertically'>
-          <Grid.Row columns={5}>
-            {[0, 1, 2, 3, 4].map(num => (
-              <Grid.Column key={num}>
-                <img src={images[num]} style={{ width: '70%' }} />
-                <Button.Group>
-                  <Button icon='plus' onClick={this.handleShow} />
-                  <Button icon='minus' onClick={this.handleHide} />
-                </Button.Group>
-              </Grid.Column>
-            ))}
-          </Grid.Row>
-          <div style={{ width: '100%' }}>
-            <h1 style={{ textAlign: 'center' }} width={'100%'}> In exchange for </h1>
-          </div>
-          <Grid.Row columns={5}>
-            {[0, 1, 2, 3, 4].map(num => (
-              <Grid.Column key={num}>
-                <img src={images[num]} style={{ width: '70%' }} />
-                <Button.Group>
-                  <Button icon='plus' onClick={this.handleShow} />
-                  <Button icon='minus' onClick={this.handleHide} />
-                </Button.Group>
-              </Grid.Column>
-            ))}
-          </Grid.Row>
-        </Grid>
-        <Modal.Actions>
-          <Button color='blue' inverted onClick={this.handleSubmit}>
-            Submit
+  handleOpen = () => this.setState({ open: true })
+  handleClose = () => this.setState({ open: false })
+
+  render(){
+    return (
+      <Button onClick={this.handleOpen} style={{width: '49%', height: '75%'}}>Trade
+        <Modal
+          open={this.state.open}
+          onClose={this.handleClose}>
+          <h1 style={{ textAlign: 'center' }}> I am offering </h1>
+          <Grid divided='vertically'>
+            <Grid.Row columns={5}>
+              {[0, 1, 2, 3, 4].map(num => (
+                <Grid.Column key={num}>
+                  <img src={images[num]} style={{ width: '70%' }} />
+                  <Button.Group>
+                    <Button icon='plus' onClick={this.handleShow} />
+                    <Button icon='minus' onClick={this.handleHide} />
+                  </Button.Group>
+                </Grid.Column>
+              ))}
+            </Grid.Row>
+            <div style={{ width: '100%' }}>
+              <h1 style={{ textAlign: 'center' }} width={'100%'}> In exchange for </h1>
+            </div>
+            <Grid.Row columns={5}>
+              {[0, 1, 2, 3, 4].map(num => (
+                <Grid.Column key={num}>
+                  <img src={images[num]} style={{ width: '70%' }} />
+                  <Button.Group>
+                    <Button icon='plus' onClick={this.handleShow} />
+                    <Button icon='minus' onClick={this.handleHide} />
+                  </Button.Group>
+                </Grid.Column>
+              ))}
+            </Grid.Row>
+          </Grid>
+          <Modal.Actions>
+            <Button color='blue' inverted onClick={this.handleClose}>
+              Submit
+        </Button>
+            <Button color='blue' inverted onClick={this.handleClose}>
+              Cancel
+        </Button>
+          </Modal.Actions>
+        </Modal>
       </Button>
-          <Button color='blue' inverted onClick={this.handleCancel}>
-            Cancel
-      </Button>
-        </Modal.Actions>
-      </Modal>
-  );
-};
-
-const mapStateToProps = (state) => {
-  return {
-
+    )
   }
 }
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    handleCancel: () => {
-
-    },
-    handleSubmit: () => {
-
-    },
-  }
-}
-export default connect(mapStateToProps, mapDispatchToProps)(TradeBtn);
+export default connect()(TradeBtn);
 
