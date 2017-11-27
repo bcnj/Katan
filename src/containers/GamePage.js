77756171139c8ba4f8b1465908bc6386d733f444
Jerry <jerrymuzsik@icloud.com>
2017-11-27 15:53:53 -0500

    fix

--- src/containers/GamePage.js
+++ src/containers/GamePage.js
@@ -37,18 +37,28 @@ class GamePage extends Component {
   }
 
   render() {
-
-      //controls which panel tab appears based on menu selection
+    //controls which panel tab appears based on menu selection
 
     let section
 
     if (this.state.activeItem === 'players') {
       section = (
         <PlayerTab
+          currentGame={this.props.currentGame}
+          gameId={this.props.gameId}
+        />
+      )
+    }
+    if (this.state.activeItem === 'messages') {
+      section = (
+        <MessageTab
+          players={this.props.currentGame.players}
+          messageCount={this.props.currentGame.game.messageCount}
           messageStart={this.props.currentGame.game.messageStart}
         />
       )
     }
+
     if (this.state.activeItem === 'log') {
       section = <LogTab />
     }
@@ -137,7 +147,7 @@ class GamePage extends Component {
                 ) : (
                   <BuildBtnInit gameId={gameId} currentGame={currentGame} />
                 ))}
-              <TradeBtn currentGame={currentGame} gameId={gameId}/>
+              <TradeBtn currentGame={currentGame} gameId={gameId} />
             </Grid.Row>
 
             <Grid.Row style={{ height: '50%' }}>
@@ -151,8 +161,11 @@ class GamePage extends Component {
             </Grid.Row>
           </Grid.Column>
         </Grid.Row>
-        { currentGame && currentGame.players && currentGame.players[localStorage.getItem(gameId)].trade &&
-        <TradePrompt currentGame={currentGame} gameId={gameId}/>}
+        {currentGame &&
+          currentGame.players &&
+          currentGame.players[localStorage.getItem(gameId)].trade && (
+            <TradePrompt currentGame={currentGame} gameId={gameId} />
+          )}
       </Grid>
     )
   }
