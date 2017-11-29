63808d90a8f5738208b2f01d565415939ac7705f
Bryan Le <bryan@bryanle.io>
2017-11-29 12:44:18 -0500

    refactor: removed tests

--- src/components/tests/LogTab.test.js
+++ src/components/tests/LogTab.test.js
@@ -1,10 +1,23 @@
-import { configure, mount, render, shallow } from 'enzyme'
+import { configure, mount, render, shallow, contain } from 'enzyme'
+// import chai from 'chai'
+// import chaiEnzyme from 'chai-enzyme'
+import { expect } from 'chai'
+
 import React from 'react'
 import Adapter from 'enzyme-adapter-react-16'
-import LogTab from '../LogTab'
-
+import LogTab from '../LogTab.jsx'
 configure({ adapter: new Adapter() })
 
 it('renders without crashing', () => {
   shallow(<LogTab />)
+})
+
+it('should contain 1 child', () => {
+  const wrapper = shallow(<LogTab />)
+  expect(wrapper.length).to.be.equal(1)
+})
+
+it('should contain a Segment', () => {
+  const wrapper = shallow(<LogTab />)
+  expect(wrapper).to.contain(Segment)
 })
--- src/containers/tests/Lobby.test.js
+++
@@ -1,9 +0,0 @@
-import { configure, mount, render, shallow } from 'enzyme'
-import React from 'react'
-import Adapter from 'enzyme-adapter-react-16'
-import { Lobby } from '../Lobby'
-configure({ adapter: new Adapter() })
-
-it('renders without crashing', () => {
-  shallow(<Lobby />)
-})
